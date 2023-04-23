9cbfdee1e625de90bedeb68422e865fd4e8648e4
Autumn Toney <at1120@georgetown.edu>
2023-04-23 15:39:23 -0400

    adding better error handling

--- main.py
+++ main.py
@@ -2,46 +2,54 @@ from database import Database
 from parser import Parser
 import sqlvalidator
 
+def main_routine(parser, db):
+    print(">>> ", end="")
+    command = input()
+    if command == "exit":
+        return "quit"
+
+    arr = command.split()
+    if arr[0] == "source":
+        file = open(arr[1], "r")
+        for x in file:
+            print(x)
+            parsed_query = p.parse_query(x)
+            db.handle(parsed_query)
+        file.close()
+
+    else:
+        """ 
+        s = sqlvalidator.parse(command)
+        if not s.is_valid():
+            print("wrong sql command")
+        else:
+            parsed_query = p.parse_query(command)
+            #db.handle(parsed_query)
+            #print(parsed_query)
+            # print(command)
+        """
+        parsed_query = parser.parse_query(command)
+        #print(parsed_query)
+        db.handle(parsed_query)
+
+    return "continue"
+
 
 if __name__ == '__main__':
 
     db = Database("project3")
     p = Parser()
-    while True:
-        command = input()
-        if command == "exit":
-            break
 
-        arr = command.split()
-        if arr[0] == "source":
-            file = open(arr[1], "r")
-            for x in file:
-                print(x)
-                parsed_query = p.parse_query(x)
-                db.handle(parsed_query)
-            file.close()
+    cont = True
 
-        else:
-            """ 
-            s = sqlvalidator.parse(command)
-            if not s.is_valid():
-                print("wrong sql command")
-            else:
-                parsed_query = p.parse_query(command)
-                #db.handle(parsed_query)
-                #print(parsed_query)
-                # print(command)
-            """
-            parsed_query = p.parse_query(command)
-            #print(parsed_query)
-            db.handle(parsed_query)
-
-
-
-
-
-
-
+    while cont:
+        try:
+            if main_routine(p, db) == "quit":
+                cont = False
+        except Exception as e:
+            print(f"Error: {e}")
+            continue
+        
 
     # Instantiate a parser
     # Define a list for parsed query
